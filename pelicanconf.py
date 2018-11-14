21c46af3053e290585d7b4ea5d3f957cd2ab396a
mutterberg <Marissa@UtterbergDataDev.com>
2018-11-13 19:58:23 -0500

    Add themes and update pelicanconf settings accordingly

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,8 +5,23 @@ from __future__ import unicode_literals
 AUTHOR = 'Marissa Utterberg'
 SITENAME = 'Utterberg Data & Development'
 SITEURL = ''
+# FAVICON = SITEURL + '/images/favicon.ico'
+COPYRIGHT_YEAR = 2018
+
+THEME = 'html5-dopetropes'
 
 PATH = 'content'
+
+STATIC_PATHS = ['extras', 'images']
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/custom.css'},
+}
+CUSTOM_CSS = 'static/custom.css'
+
+PLUGINS_PATHS = []
+PLUGINS = []
+
+ARTICLE_PATHS = ['articles',]
 
 TIMEZONE = 'America/New_York'
 
@@ -20,16 +35,28 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Demo1', 'http://getpelican.com/'),
+         ('Demo2', 'http://python.org/'),
+         ('Demo3', 'http://jinja.pocoo.org/'),
+         ('Demo4', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://www.github.com/mutterberg/'),
+          ('twitter', 'https://www.twitter.com/mutterberg/'),)
 
 DEFAULT_PAGINATION = 10
+
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa'
+}
+
+# DEFAULT_METADATA = {
+#     'status': 'draft',
+# }
+
+DELETE_OUTPUT_DIRECTORY = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
